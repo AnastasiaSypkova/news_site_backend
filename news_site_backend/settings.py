eabc08c762689da75a9efb0899bee7ca80cd8ae6
Anastasia Sypkova <a.sypkova@tg.dunice.net>
2024-05-13 11:48:37 +0300

    add auth user model

--- news_site_backend/settings.py
+++ news_site_backend/settings.py
@@ -134,6 +134,8 @@ MEDIA_URL = "/media/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+AUTH_USER_MODEL = "users.Users"
+
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
