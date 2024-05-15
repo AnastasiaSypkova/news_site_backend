663973006ef9996833f0c4e33d50ab7d8b8b33c4
Anastasia Sypkova <a.sypkova@tg.dunice.net>
2024-05-15 16:07:24 +0300

    make email field as username_field

--- users/models.py
+++ users/models.py
@@ -10,8 +10,10 @@ class Users(AbstractUser):
     avatar_path = models.ImageField(upload_to=upload_to, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    email = models.EmailField(unique=True)
 
-    REQUIRED_FIELDS = ["email", "password"]
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["password"]
 
     class Meta:
         ordering = ["id"]
