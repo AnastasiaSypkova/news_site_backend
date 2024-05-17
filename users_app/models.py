37a258b2f6afc352c3ca4fd4602d8a78054cd64d
Anastasia Sypkova <a.sypkova@tg.dunice.net>
2024-05-17 11:24:58 +0300

    make users password hashing in despite of the way they were created whether by admin panel or by post request

--- users_app/models.py
+++ users_app/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
@@ -20,6 +21,13 @@ class MyUser(AbstractUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["password"]
+
+    def save(self, *args, **kwargs):
+        if self.password and not self.password.startswith(
+            ("pbkdf2_sha256$", "bcrypt$", "argon2")
+        ):
+            self.password = make_password(self.password)
+        super().save(*args, **kwargs)
 
     class Meta:
         ordering = ["id"]
--- users_app/serializers.py
+++ users_app/serializers.py
@@ -6,7 +6,12 @@ from users_app.models import MyUser
 
 class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
-        validated_data["password"] = make_password(validated_data["password"])
+        if not validated_data["password"].startswith(
+            ("pbkdf2_sha256$", "bcrypt$", "argon2")
+        ):
+            validated_data["password"] = make_password(
+                validated_data["password"]
+            )
         return super(UserSerializer, self).create(validated_data)
 
     class Meta:
