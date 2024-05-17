348700c78e264f595153c470b61976bc0fd120c2
Anastasia Sypkova <a.sypkova@tg.dunice.net>
2024-05-17 10:56:57 +0300

    place user manager into a separate file

---
+++ users_app/managers.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+class MyUserManager(BaseUserManager):
+    """
+    User manager where email is the unique identifier
+    instead of username
+    """
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Private method to create user and save it to db
+        """
+        if not email:
+            raise ValueError("Users must have an email address")
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """
+        Creates and saves a superuser with the given email and password.
+        """
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True")
+
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True")
+        return self._create_user(email, password, **extra_fields)
