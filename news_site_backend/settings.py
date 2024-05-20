7696188acb0ff443e07f45b340b408218df9783e
Anastasia Sypkova <a.sypkova@tg.dunice.net>
2024-05-20 15:23:35 +0300

    change settings in order to avoid merge conflict

--- news_site_backend/settings.py
+++ news_site_backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -40,6 +41,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "users_app",
+    "rest_framework_simplejwt",
 ]
 
 MIDDLEWARE = [
@@ -123,6 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
