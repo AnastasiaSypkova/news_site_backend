50024563f671d5815d791d087326f208081ab0ef
Anastasia Sypkova <a.sypkova@tg.dunice.net>
2024-05-21 14:47:04 +0300

    permissions were added to PostsViewSets

--- posts_app/views.py
+++ posts_app/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
+from news_site_backend.permissions import ReadOnly
 from posts_app.models import Posts
 from posts_app.serializers import PostSerializer
 
@@ -8,8 +9,11 @@ from posts_app.serializers import PostSerializer
 class PostsViewSet(viewsets.ModelViewSet):
     """
     Viewset for endpoints assotiated with posts model
+
+    Only authenticated users can add news
+    All users can read news
     """
 
     queryset = Posts.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated | ReadOnly,)
