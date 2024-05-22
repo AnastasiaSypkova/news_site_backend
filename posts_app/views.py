24a59ae39ba5d4b4135b089311ed4ec59d0e2d03
Anastasia Sypkova <a.sypkova@tg.dunice.net>
2024-05-22 10:17:28 +0300

    filtering by query params was implemented

--- posts_app/views.py
+++ posts_app/views.py
@@ -14,6 +14,27 @@ class PostsViewSet(viewsets.ModelViewSet):
     All users can read news
     """
 
-    queryset = Posts.objects.all()
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticated | ReadOnly, EditeOwnPost | ReadOnly]
+
+    def get_queryset(self):
+        """
+        implement filtering against query set parameters
+
+        Filtering by author id, author first and last name,
+        author email, and text to search in post title and post text
+        """
+        queryset = Posts.objects.all()
+        author = self.request.query_params.get("author")
+        author_query_id = self.request.query_params.get("authorId")
+        email = self.request.query_params.get("email")
+        if author:
+            queryset = queryset.filter(author__first_name=author)
+            if not queryset:
+                queryset = queryset.filter(author__last_name=author)
+        if author_query_id:
+            queryset = queryset.filter(author_id=author_query_id)
+        if email:
+            queryset = queryset.filter(author__email=email)
+
+        return queryset
