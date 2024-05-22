e5ef617891b9267e03285ff93ec756dc1639ea58
Anastasia Sypkova <a.sypkova@tg.dunice.net>
2024-05-22 12:24:53 +0300

    fix pathes in urlpatterns

--- posts_app/urls.py
+++ posts_app/urls.py
@@ -4,6 +4,6 @@ from rest_framework import routers
 from posts_app.views import PostsViewSet
 
 router = routers.DefaultRouter()
-router.register(r"posts", PostsViewSet, basename="posts")
+router.register("", PostsViewSet, basename="posts")
 
 urlpatterns = [path("", include(router.urls))]
