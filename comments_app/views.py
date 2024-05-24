9087f1d17e40cac2568441940277ab622f1dddf7
Anastasia Sypkova <a.sypkova@tg.dunice.net>
2024-05-24 11:05:40 +0300

    implement comments filtering by post id

--- comments_app/views.py
+++ comments_app/views.py
@@ -1,9 +1,24 @@
-from rest_framework import viewsets
+from rest_framework import filters, viewsets
 from rest_framework.permissions import IsAuthenticated
 
 from comments_app.models import Comments
 from comments_app.serializers import CommentsSerializer
 from news_site_backend.permissions import EditeOwnObject, ReadOnly
+
+
+class FilterByPostIdBackend(filters.BaseFilterBackend):
+    """
+    implement filtering against query set parameters
+
+    Filtering comments by post id
+    """
+
+    def filter_queryset(self, request, queryset, view):
+        queryset = Comments.objects.all()
+        post_id = request.query_params.get("post")
+        if post_id:
+            queryset = queryset.filter(post__id=post_id)
+        return queryset
 
 
 class CommentsViewSet(viewsets.ModelViewSet):
@@ -16,3 +31,5 @@ class CommentsViewSet(viewsets.ModelViewSet):
     serializer_class = CommentsSerializer
     queryset = Comments.objects.all()
     permission_classes = [IsAuthenticated, EditeOwnObject | ReadOnly]
+    filter_backends = [FilterByPostIdBackend]
+    filterset_fields = ["post"]
