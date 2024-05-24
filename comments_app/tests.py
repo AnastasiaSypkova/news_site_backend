ad6d100d502198c431020ecfcdb6149252513c22
Anastasia Sypkova <a.sypkova@tg.dunice.net>
2024-05-24 16:02:38 +0300

    tests for comments endpoints were implemented

--- comments_app/tests.py
+++ comments_app/tests.py
@@ -23,9 +23,12 @@ class CommentsApiTest(APITestCase):
         self.user = MyUser.objects.create_user(
             "authenticated_usere@mail.ru", "password"
         )
+        self.second_user = MyUser.objects.create_user(
+            "second_user@mail.ru", "password"
+        )
         self.client.force_authenticate(user=self.user)
 
-    def create_post(self):
+    def create_post(self) -> int:
         """
         Helper function that creates post for commenting
 
@@ -47,6 +50,20 @@ class CommentsApiTest(APITestCase):
         post_id = response.data["id"]
         return post_id
 
+    def create_comment(self, post_id: int) -> int:
+        """
+        Helper function that creates comment to the post
+
+        Returns comment id
+        """
+        comment_data = {"text": "текст комментария", "post_id": post_id}
+        create_comment_response = self.client.post(
+            self.base_url, comment_data, format="json"
+        )
+
+        comment_id = create_comment_response.data["id"]
+        return comment_id
+
     def test_create_comment_for_existing_post(self):
         """
         Test for comment creating for specific post with certain id
@@ -56,3 +73,88 @@ class CommentsApiTest(APITestCase):
         response = self.client.post(self.base_url, comment_data, format="json")
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_comment_for_non_existing_post(self):
+        """
+        Test creating comment for non valid id
+        """
+        post_id = 351
+
+        comment_data = {"text": "текст комментария", "post_id": post_id}
+        response = self.client.post(self.base_url, comment_data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_get_comment(self):
+        """
+        Ensure we can read all comments by GET request
+        """
+        status_code = self.client.get(self.base_url).status_code
+        self.assertEqual(status_code, status.HTTP_200_OK)
+
+    def test_edit_comment(self):
+        """
+        Ensure the comment author can edit his comment
+        """
+        post_id = self.create_post()
+        comment_id = self.create_comment(post_id)
+        edited_comment_data = {
+            "text": "отредактированный текст комментария",
+            "post_id": post_id,
+        }
+        edit_comment_response = self.client.patch(
+            f"{self.base_url} {comment_id}/",
+            edited_comment_data,
+            format="json",
+        )
+        self.assertEqual(edit_comment_response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            edit_comment_response.data["text"],
+            "отредактированный текст комментария",
+        )
+
+    def test_edit_another_user_comment(self):
+        """
+        Ensure that user can't edit anothers users comments
+        """
+        post_id = self.create_post()
+        comment_id = self.create_comment(post_id)
+
+        self.client.force_authenticate(user=self.second_user)
+        edited_comment_data = {
+            "text": "пытаемся отредактировать чужой комментарий",
+            "post_id": post_id,
+        }
+        edit_comment_response = self.client.patch(
+            f"{self.base_url} {comment_id}/",
+            edited_comment_data,
+            format="json",
+        )
+        self.assertEqual(
+            edit_comment_response.status_code, status.HTTP_403_FORBIDDEN
+        )
+
+    def test_delete_comment(self):
+        """
+        Ensure that comment author can delete its comment
+        """
+        post_id = self.create_post()
+        comment_id = self.create_comment(post_id)
+
+        response = self.client.delete(
+            f"{self.base_url} {comment_id}/", format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_delete_other_comment(self):
+        """
+        Ensure that user can't delete anothers users comments
+        """
+        post_id = self.create_post()
+        comment_id = self.create_comment(post_id)
+
+        self.client.force_authenticate(user=self.second_user)
+        response = self.client.delete(
+            f"{self.base_url} {comment_id}/", format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
